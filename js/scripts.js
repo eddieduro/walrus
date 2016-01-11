87d082571fa91fd8e753fe2b93309c4746defc6e
Eddie Duro and Jason Wuerch <wuerchjason@gmail.com>
2016-01-11 14:57:06 -0800

    lots of changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,22 @@
 $(function() {
-  $('h1').click(function() {
-    alert("I am a header!")
+
+
+  $(".js-div").mouseenter(function() {
+    $("#1>h3").fadeOut(1000)
   });
+  $(".js-div").mouseleave(function() {
+    $("#1>h3").fadeIn(3000);
+  });
+
+
+  $(".js-div").mouseenter(function() {
+    $("#1>h4").fadeIn(6000).animate({opacity: 1}, 5000);
+  });
+  $(".js-div").mouseleave(function() {
+    $("#1>h4").fadeOut(100).animate({opacity: 0}, 10);
+  });
+
+
+
+
 });
